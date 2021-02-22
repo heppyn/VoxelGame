b7c7d6238b0abed29ba030bb75ccc3b7ef95936f
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-02-22 14:44:42 +0100

    Add renderer

--- VoxelGame/src/Renderer.h
+++ VoxelGame/src/Renderer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <GL/glew.h>
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
@@ -12,5 +15,9 @@ bool GLLogCall(const char* function, const char* file, int line);
 
 class Renderer
 {
-};
+private:
 
+public:
+    void Clear() const;
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
