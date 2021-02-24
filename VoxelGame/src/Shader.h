2ad5edf5ba7cfa171fc58afdb356a3b75c3100de
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-02-24 17:09:03 +0100

    Add textures

--- VoxelGame/src/Shader.h
+++ VoxelGame/src/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include "glm/glm.hpp"
 
 struct ShaderProgramSource {
 	std::string VertexSource;
@@ -21,8 +22,10 @@ public:
 	void Unbind() const;
 
 	// Set uniforms
+	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 
 private:
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
