8fa9765986c22150f8f99cfd8ad8c428204a8ca0
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-02-24 17:09:19 +0100

    Add im gui

--- VoxelGame/src/Application.cpp
+++ VoxelGame/src/Application.cpp
@@ -11,6 +11,13 @@
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
 #include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 
 int main(void)
@@ -26,7 +33,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(690, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -46,10 +53,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
     { // create scope to release buffers before glfwTerminate
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f,
+            100.0f, 100.0f, 0.0f, 0.0f,
+            400.0f, 100.0f, 1.0f, 0.0f,
+            400.0f, 400.0f, 1.0f, 1.0f,
+            100.0f, 400.0f, 0.0f, 1.0f,
         };
 
         unsigned int indecies[] = {
@@ -57,22 +64,40 @@ int main(void)
             2, 3, 0,
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         unsigned int vao;
         GLCall(glGenVertexArrays(1, &vao));
         GLCall(glBindVertexArray(vao));
 
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
         IndexBuffer ib(indecies, 6);
 
-        Shader shader("res/shaders/Basic.shader");
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        // Move camera to the right by moving objects to the left
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        // transform of an object
+        //glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        //// column major
+        //glm::mat4 mvp = proj * view * model;
+
+        Shader shader("res/shaders/Texture.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        /*shader.SetUniformMat4f("u_MVP", mvp);*/
+
+        Texture texture("res/textures/doge.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         vb.Unbind();
@@ -80,6 +105,12 @@ int main(void)
         shader.Unbind();
 
         Renderer renderer;
+
+        ImGui::CreateContext();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui::StyleColorsDark();
+
+        glm::vec3 translation(200, 200, 0);
 
         float r = .0f;
         float increment = 0.05f;
@@ -89,8 +120,16 @@ int main(void)
             /* Render here */
             renderer.Clear();
 
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+			glm::mat4 mvp = proj * view * model;
+
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(va, ib, shader);
 
@@ -101,6 +140,18 @@ int main(void)
 
             r += increment;
 
+			{
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+				
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -108,6 +159,10 @@ int main(void)
             glfwPollEvents();
         }
     }
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
