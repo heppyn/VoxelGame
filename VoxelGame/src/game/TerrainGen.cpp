c0420862be6f067b368a186bd0c9391bd7bdfaab
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-06-17 18:08:54 +0200

    Update terrain generation

--- VoxelGame/src/game/TerrainGen.cpp
+++ VoxelGame/src/game/TerrainGen.cpp
@@ -1,7 +1,6 @@
 #include "TerrainGen.h"
 
-#include <cstdlib>
-#include <ctime>
+#include <perlin_noise/PerlinNoise.hpp>
 
 #include "engine/ResourceManager.h"
 
@@ -9,26 +8,17 @@
 std::vector<GameObject> Terrain::TerrainGen::GenerateChunk(const glm::vec2& position) {
     std::vector<GameObject> res;
     res.reserve(ChunkSize * ChunkSize);
-    std::srand(std::time(nullptr));
-    const std::vector<Renderer::Texture2D*> textures = {
-        ResourceManager::GetTexture2D("box"),
-        ResourceManager::GetTexture2D("boxSpec"),
-    };
-    auto* concrete = ResourceManager::GetTexture2D("concrete");
+    const siv::BasicPerlinNoise<float> perlin(0);
 
     for (unsigned int i = 0; i < ChunkSize; ++i) {
         for (unsigned int j = 0; j < ChunkSize; ++j) {
-            const auto r = std::rand() / ((RAND_MAX + 1u) / 10);
+            const auto h = perlin.noise2D(
+              (position.x + static_cast<float>(i)) / static_cast<float>(ChunkSize),
+              (position.y + static_cast<float>(j)) / static_cast<float>(ChunkSize));
             res.emplace_back(
-              glm::vec3(position.x + static_cast<float>(i), 0.0f, position.y + static_cast<float>(j)),
-              glm::vec2(0, 1),
+              glm::vec3(position.x + static_cast<float>(i), std::floor(h * 10), position.y + static_cast<float>(j)),
+              glm::vec2(0, 0),
               true);
-            if (r % 10 == 0) {
-                res.emplace_back(
-                  glm::vec3(position.x + static_cast<float>(i), 1.0f, position.y + static_cast<float>(j)),
-                  glm::vec2(0, 0),
-                  true);
-            }
         }
     }
 
