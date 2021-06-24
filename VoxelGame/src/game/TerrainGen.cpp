3060126e7813a86b9ebf32ea98eb5a10438c0345
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-06-24 18:26:20 +0200

    Update terrain gen

--- VoxelGame/src/game/TerrainGen.cpp
+++ VoxelGame/src/game/TerrainGen.cpp
@@ -22,9 +22,10 @@ Chunk Terrain::TerrainGen::GenerateChunk(const glm::vec2& position) {
 
     for (unsigned int i = 0; i < chunkSize; ++i) {
         for (unsigned int j = 0; j < chunkSize; ++j) {
-            const auto h = perlin.noise2D(
+            const auto h = perlin.normalizedOctaveNoise2D(
               (position.x + static_cast<float>(i)) / Chunk::ChunkSize,
-              (position.y + static_cast<float>(j)) / Chunk::ChunkSize);
+              (position.y + static_cast<float>(j)) / Chunk::ChunkSize,
+                2);
             objects.emplace_back(
               BlockFactory::CreateBlock(
                 glm::vec3(position.x + static_cast<float>(i), std::floor(h * 10), position.y + static_cast<float>(j)),
