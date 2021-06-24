d9b90c618ce67b08ee9745991d2fbdc2a9ff26e5
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-06-24 18:21:56 +0200

    Update background color

--- VoxelGame/src/renderer/SceneRenderer.cpp
+++ VoxelGame/src/renderer/SceneRenderer.cpp
@@ -13,7 +13,7 @@ void Renderer::SceneRenderer::Render(const Scene& scene, unsigned width, unsigne
     BindInstancesData(scene);
 
     // TODO: disable this for rendering multiple scenes
-    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glClearColor(103/255.0f, 157/255.0f, 245/255.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     std::vector<Shader*> shaders = {
