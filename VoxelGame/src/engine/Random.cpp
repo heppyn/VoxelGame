9e6d49fe57aa4079d3583e0e9da84c36aa6af205
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-09-26 14:54:27 +0200

    Add noise methods

--- VoxelGame/src/engine/Random.cpp
+++ VoxelGame/src/engine/Random.cpp
@@ -1,5 +1,7 @@
 #include "Random.h"
 
+#include "helpers/Math.h"
+
 siv::BasicPerlinNoise<float> Engine::Random::Perlin{};
 unsigned Engine::Random::Seed{ 0 };
 std::random_device Engine::Random::RandomGen{};
@@ -11,4 +13,44 @@ void Engine::Random::Init() {
 void Engine::Random::Init(unsigned seed) {
     Seed = seed;
     Perlin.reseed(Seed);
+}
+
+bool Engine::Random::IsLocalMaxPerlin(const glm::vec2& pos, float freq, int octaves) {
+    const auto r = Perlin.accumulatedOctaveNoise2D_0_1(pos.x / freq, pos.y / freq, octaves);
+    const auto r1 = Perlin.accumulatedOctaveNoise2D_0_1((pos.x + 1) / freq, pos.y / freq, octaves);
+    const auto r2 = Perlin.accumulatedOctaveNoise2D_0_1((pos.x - 1) / freq, pos.y / freq, octaves);
+    const auto r3 = Perlin.accumulatedOctaveNoise2D_0_1(pos.x / freq, (pos.y + 1) / freq, octaves);
+    const auto r4 = Perlin.accumulatedOctaveNoise2D_0_1(pos.x / freq, (pos.y - 1) / freq, octaves);
+                    
+    const auto r5 = Perlin.accumulatedOctaveNoise2D_0_1((pos.x + 1) / freq, (pos.y + 1) / freq, octaves);
+    const auto r6 = Perlin.accumulatedOctaveNoise2D_0_1((pos.x - 1) / freq, (pos.y + 1) / freq, octaves);
+    const auto r7 = Perlin.accumulatedOctaveNoise2D_0_1((pos.x + 1) / freq, (pos.y - 1) / freq, octaves);
+    const auto r8 = Perlin.accumulatedOctaveNoise2D_0_1((pos.x - 1) / freq, (pos.y - 1) / freq, octaves);
+
+    return std::max({ r, r1, r2, r3, r4, r5, r6, r7, r8 }) == r;
+}
+
+unsigned Engine::Random::GetSquirrel1dNoise(int x, unsigned seed) {
+    // Squirrel noise 3, taken from
+    // https://www.youtube.com/watch?v=LWFzPP8ZbdU
+    constexpr unsigned int BIT_NOISE1 = 0x68E31DA4;
+    constexpr unsigned int BIT_NOISE2 = 0xB5297A4D;
+    constexpr unsigned int BIT_NOISE3 = 0x1B56C4E9;
+
+    auto mangledBits = static_cast<unsigned int>(x);
+    mangledBits *= BIT_NOISE1;
+    mangledBits += seed;
+    mangledBits ^= (mangledBits >> 8);
+    mangledBits += BIT_NOISE2;
+    mangledBits ^= (mangledBits << 8);
+    mangledBits *= BIT_NOISE3;
+    mangledBits ^= (mangledBits >> 8);
+    return mangledBits;
+}
+
+unsigned Engine::Random::GetSquirrel2dNoise(int x, int y, unsigned seed) {
+    return Get1dNoise(x + PRIME1 * y, seed);
+}
+unsigned Engine::Random::GetSquirrel3dNoise(int x, int y, int z, unsigned seed) {
+    return Get1dNoise(x + PRIME1 * y + PRIME2 * z, seed);
 }
--- VoxelGame/src/engine/Random.h
+++ VoxelGame/src/engine/Random.h
@@ -1,18 +1,56 @@
 #pragma once
 
 #include <random>
+#include <limits>
 #include <perlin_noise/PerlinNoise.hpp>
 
-namespace Engine
-{
-class Random
-{
-public:
+#include "glm/vec2.hpp"
+
+namespace Engine {
+class Random {
+  public:
     static siv::BasicPerlinNoise<float> Perlin;
     static unsigned Seed;
+    //TODO: get rid of this - not PRNG - too slow
     static std::random_device RandomGen;
 
     static void Init();
     static void Init(unsigned seed);
+    static bool IsLocalMaxPerlin(const glm::vec2& pos, float freq, int octaves);
+
+    // convert T to int. static_cast<unsigned int>(-1.0f) == 0u
+    template<typename T>
+    static unsigned int Get1dNoise(T x, unsigned int seed = Seed) {
+        return GetSquirrel1dNoise(static_cast<int>(x), seed);
+    }
+    template<typename T>
+    static unsigned int Get2dNoise(T x, T y, unsigned int seed = Seed) {
+        return GetSquirrel2dNoise(static_cast<int>(x), static_cast<int>(y), seed);
+    }
+    template<typename T>
+    static unsigned int Get3dNoise(T x, T y, T z, unsigned int seed = Seed) {
+        return GetSquirrel3dNoise(static_cast<int>(x), static_cast<int>(y), static_cast<int>(z), seed);
+    }
+
+    template<typename T, typename Y>
+    static T Get1dNoise0_1(Y x, unsigned int seed = Seed) {
+        return static_cast<T>(Get1dNoise(x, seed)) / static_cast<T>(std::numeric_limits<unsigned int>::max());
+    }
+    template<typename T, typename Y>
+    static T Get2dNoise0_1(Y x, Y y, unsigned int seed = Seed) {
+        return static_cast<T>(Get2dNoise(x, y, seed)) / static_cast<T>(std::numeric_limits<unsigned int>::max());
+    }
+    template<typename T, typename Y>
+    static T Get3dNoise0_1(Y x, Y y, Y z, unsigned int seed = Seed) {
+        return static_cast<T>(Get3dNoise(x, y, z, seed)) / static_cast<T>(std::numeric_limits<unsigned int>::max());
+    }
+
+  private:
+    static constexpr int PRIME1 = 198491317; // large prime number with non-boring bits
+    static constexpr int PRIME2 = 6542989; // large prime number with non-boring bits
+
+    static unsigned int GetSquirrel1dNoise(int x, unsigned int seed);
+    static unsigned int GetSquirrel2dNoise(int x, int y, unsigned int seed);
+    static unsigned int GetSquirrel3dNoise(int x, int y, int z, unsigned int seed);
 };
-}
+} // namespace Engine
