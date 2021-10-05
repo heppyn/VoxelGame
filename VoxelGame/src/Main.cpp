5f018d04c04800ff7831831a94420661174b1842
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-10-05 14:25:11 +0200

    Add face culling

--- VoxelGame/src/Main.cpp
+++ VoxelGame/src/Main.cpp
@@ -30,6 +30,7 @@ int main() {
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_CULL_FACE);
     game->Init();
     camera = game->GetCamera();
 
