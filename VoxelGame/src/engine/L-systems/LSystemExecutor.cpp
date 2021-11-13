20dbec04db7e6905a733484a4caac35abe1e0b0a
Lukáš Hepner <hepneluk@fit.cvut.cz>
2021-11-13 15:40:51 +0100

    Add random angle to pitch and yaw

--- VoxelGame/src/engine/L-systems/LSystemExecutor.cpp
+++ VoxelGame/src/engine/L-systems/LSystemExecutor.cpp
@@ -2,11 +2,12 @@
 
 #include "engine/GameObjectFactory.h"
 
-LSystems::LSystemExecutor::LSystemExecutor(int derivationVar) : DerivationVar(derivationVar) {}
+LSystems::LSystemExecutor::LSystemExecutor(int derivationVar) : DerivationVar_(derivationVar) {}
+LSystems::LSystemExecutor::LSystemExecutor(float randomAngle) : RandomAngle_(randomAngle) {}
 
 std::vector<GameObject> LSystems::LSystemExecutor::GenerateBasedOn(const glm::vec3& pos, const LSystem& lSystem, float scale, int numDerivations, unsigned salt) {
-    if (DerivationVar) {
-        numDerivations += static_cast<int>(Engine::Random::Get1dNoiseLimited(salt, DerivationVar));
+    if (DerivationVar_) {
+        numDerivations += static_cast<int>(Engine::Random::Get1dNoiseLimited(salt, DerivationVar_));
     }
     std::vector<GameObject> objects;
     Scale_ = scale;
@@ -16,13 +17,16 @@ std::vector<GameObject> LSystems::LSystemExecutor::GenerateBasedOn(const glm::ve
     //std::cout << lSystem.Grammar.Derivate(numDerivations, salt) << "\n\n";
 
     for (const auto production = lSystem.Grammar.Derivate(numDerivations, salt); const auto letter : production) {
-        ExecuteLetter(letter, lSystem, objects, turtle);
+        ExecuteLetter(letter, lSystem, objects, turtle, salt);
+        salt = Engine::Random::Get1dNoise(salt);
     }
 
     return objects;
 }
 
-void LSystems::LSystemExecutor::ExecuteLetter(char letter, const LSystem& lSystem, std::vector<GameObject>& objects, Detail::Turtle& turtle) {
+void LSystems::LSystemExecutor::ExecuteLetter(char letter, const LSystem& lSystem, std::vector<GameObject>& objects, Detail::Turtle& turtle, unsigned salt) {
+    // [-1.0, 1.0] * percentage
+    const auto angleDelta = (Engine::Random::Get1dNoise0_1<float>(salt) - 0.5f) * 2.0f * RandomAngle_;
     // TODO: save this in hash map if it is too slow for large alphabet
     switch (letter) {
         case 'U':
@@ -42,16 +46,16 @@ void LSystems::LSystemExecutor::ExecuteLetter(char letter, const LSystem& lSyste
             break;
         // using right hand system, keep + to match the book
         case '+':
-            turtle.Rotate(-lSystem.Yaw, 0.0f);
+            turtle.Rotate(-lSystem.Yaw + angleDelta * lSystem.Yaw, 0.0f);
             break;
         case '-':
-            turtle.Rotate(lSystem.Yaw, 0.0f);
+            turtle.Rotate(lSystem.Yaw + angleDelta * lSystem.Yaw, 0.0f);
             break;
         case '&':
-            turtle.Rotate(0.0f, -lSystem.Pitch);
+            turtle.Rotate(0.0f, -lSystem.Pitch + angleDelta * lSystem.Pitch);
             break;
         case '^':
-            turtle.Rotate(0.0f, lSystem.Pitch);
+            turtle.Rotate(0.0f, lSystem.Pitch + angleDelta * lSystem.Pitch);
             break;
         case '[':
             TStack_.emplace(turtle);
--- VoxelGame/src/engine/L-systems/LSystemExecutor.h
+++ VoxelGame/src/engine/L-systems/LSystemExecutor.h
@@ -10,13 +10,18 @@ class LSystemExecutor {
   public:
     LSystemExecutor() = default;
     explicit LSystemExecutor(int derivationVar);
+    explicit LSystemExecutor(float randomAngle);
     [[nodiscard]] std::vector<GameObject> GenerateBasedOn(const glm::vec3& pos, const LSystem& lSystem, float scale, int numDerivations, unsigned salt);
 
   private:
-    int DerivationVar{ 0 };
+    int DerivationVar_{ 0 };
+    // variation of L-system angle
+    // percentage of angle change [0.0, 1.0]
+    float RandomAngle_{ 0.2f };
+
     std::stack<Detail::Turtle> TStack_{};
     float Scale_{};
 
-    void ExecuteLetter(char letter, const LSystem& lSystem, std::vector<GameObject>& objects, Detail::Turtle& turtle);
+    void ExecuteLetter(char letter, const LSystem& lSystem, std::vector<GameObject>& objects, Detail::Turtle& turtle, unsigned salt);
 };
 } // namespace LSystems
